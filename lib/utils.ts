dcbe779377d550c56e9e9fb57a782cc279af478e
Zeinullah <zeinullah.iman@silence.codes>
2026-07-07 15:49:35 +0500

    feat: add formatMoney helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatMoney(value: number): string {
+  return Math.round(value)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " ₸";
+}
