4ce7f69270938a67e48a16d83fb1fbbf0343c882
Zeinullah <zeinullah.iman@silence.codes>
2026-08-21 12:27:14 +0500

    Add Yandex.Metrika counter

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,17 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+        {/* Yandex.Metrika (noscript) */}
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/111792617"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika (noscript) */}
         <Providers>
           <LoadingScreen />
           <ScrollProgress />
@@ -87,6 +98,20 @@ export default function RootLayout({
             gtag('config', 'G-J05Y4WBEHN');
           `}
         </Script>
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=111792617', 'ym');
+
+            ym(111792617, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        {/* /Yandex.Metrika counter */}
         <CookieConsent />
       </body>
     </html>
